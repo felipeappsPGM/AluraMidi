660cfdb3543700124a62742c43e93ee5d944c91a
Felipe Sousa Chagas <felipe.chagas@agrinorte.paragominas>
2023-05-01 00:09:06 -0300

    logica do null

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-const btnPom = document.querySelector('.tecla_pom');
-const audioPom = document.getElementById('som_tecla_pom');
-btnPom.addEventListener('click', () => {
-    tocaSomPom();
-});
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomPom(){
-    audioPom.play();
+function tocaSom(id){
+    const elemento = document.querySelector(id);
+    if(elemento.localName === 'audio' && elemento !== null){
+        
+        elemento.play();
+        
+    }else{
+        console.log('elemento não encontrado');
+    }
+    
+}
+
+for(let i = 0; i < listaDeTeclas.length; i++){
+    const param = `#som_${listaDeTeclas[i].classList[1]}`;
+    listaDeTeclas[i].onclick = () => {tocaSom(param);}
+
+    listaDeTeclas[i].onkeydown = (evento) => {
+        if(evento.code === 'Space'|| evento.code === 'Enter')
+        listaDeTeclas[i].classList.add('ativa');
+    }
+
+    listaDeTeclas[i].onkeyup = () => {
+        listaDeTeclas[i].classList.remove('ativa');
+    }
 }
